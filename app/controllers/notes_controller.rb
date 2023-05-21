d3ddba66e4c5c14e1bf4531035fdfd089ad5cbcd
Bill Blakney <bblakney99@gmail.com>
2023-05-21 10:28:06 -0400

    + add pagination + update test fixtures for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,7 @@
 class NotesController < ApplicationController
 
   def index
-    @notes = Note.all.order('notes.display_at DESC').all
+    @notes = Note.page(params[:page]).per(10)
   end
 
   def show
