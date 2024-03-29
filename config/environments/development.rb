1ef620afb2dd7b15a80c8dbb3f829afe442ea45c
Bill Blakney <bblakney99@gmail.com>
2024-03-29 10:01:58 -0400

    POC: introduce svg; replace table action buttons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "notes.blak.myds.me"
+
+  config.assets.precompile += %w( '.svg' )
 end
