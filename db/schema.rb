40e3940925356415e5cc2827bc3fdb94010c2dca
Bill Blakney <bblakney99@gmail.com>
2025-01-05 10:11:30 -0500

    updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_05_211355) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "notes", force: :cascade do |t|
     t.text "text"
     t.text "detail"
